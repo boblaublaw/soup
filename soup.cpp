ae6c805b5bb3a947db9193beb07a850da29d4b1b
boblaublaw <github@sublimation.org>
2015-07-14 15:54:44 -0700

    helper functions taking shape

--- soup.cpp
+++ soup.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <string>
-#include <map>
+#include <string>       // string
+#include <ctype.h>      // tolower
+#include <map>          // map
 
 #include <unistd.h>
 
@@ -8,42 +9,43 @@ using namespace std;
 #define NUMCHARS 26
 
 map<unsigned char,string> numberNames;
+int counter;
 
 void init_number_names(void)
 {
-    numberNames[90]="ninety";
-    numberNames[80]="eighty";
-    numberNames[70]="seventy";
-    numberNames[60]="sixty";
-    numberNames[50]="fifty";
-    numberNames[40]="forty";
-    numberNames[30]="thirty";
-    numberNames[20]="twenty";
-    numberNames[19]="nineteen";
-    numberNames[18]="eighteen";
-    numberNames[17]="seventeen";
-    numberNames[16]="sixteen";
-    numberNames[15]="fifteen";
-    numberNames[14]="fourteen";
-    numberNames[13]="thirteen";
-    numberNames[12]="twelve";
-    numberNames[11]="eleven";
-    numberNames[10]="ten";
-    numberNames[9]="nine";
-    numberNames[8]="eight";
-    numberNames[7]="seven";
-    numberNames[6]="six";
-    numberNames[5]="five";
-    numberNames[4]="four";
-    numberNames[3]="three";
-    numberNames[2]="two";
-    numberNames[1]="one";
-    numberNames[0]="zero";
+    numberNames[90] = "ninety";
+    numberNames[80] = "eighty";
+    numberNames[70] = "seventy";
+    numberNames[60] = "sixty";
+    numberNames[50] = "fifty";
+    numberNames[40] = "forty";
+    numberNames[30] = "thirty";
+    numberNames[20] = "twenty";
+    numberNames[19] = "nineteen";
+    numberNames[18] = "eighteen";
+    numberNames[17] = "seventeen";
+    numberNames[16] = "sixteen";
+    numberNames[15] = "fifteen";
+    numberNames[14] = "fourteen";
+    numberNames[13] = "thirteen";
+    numberNames[12] = "twelve";
+    numberNames[11] = "eleven";
+    numberNames[10] = "ten";
+    numberNames[9] = "nine";
+    numberNames[8] = "eight";
+    numberNames[7] = "seven";
+    numberNames[6] = "six";
+    numberNames[5] = "five";
+    numberNames[4] = "four";
+    numberNames[3] = "three";
+    numberNames[2] = "two";
+    numberNames[1] = "one";
+    numberNames[0] = "zero";
 }
 
 string convert_number_names(unsigned char input) 
 {
-    string output="";
+    string output = "";
     for(map<unsigned char, string>::reverse_iterator iterator = numberNames.rbegin(); iterator != numberNames.rend(); iterator++) {
         unsigned char key=iterator->first;
         //cout << "checking " << to_string(input) << " against " << to_string(key) << endl;
@@ -63,8 +65,8 @@ string convert_number_names(unsigned char input)
 
 string gen_string(unsigned char counts[NUMCHARS])
 {
-    string output="";
-    char a='a';
+    string output = "";
+    char a = 'a';
 
     for (unsigned char i=0; i<NUMCHARS; i++) {
         output.append(convert_number_names(counts[i]));
@@ -77,7 +79,22 @@ string gen_string(unsigned char counts[NUMCHARS])
 
 bool count_chars(string input, unsigned char counts[NUMCHARS])
 {
-    return false;
+    string::iterator it = input.begin();
+    int index = 0;
+    char charIndex;
+
+    for (index=0; index < NUMCHARS; index++) 
+        counts[index]=0;
+
+    for (it = input.begin() ; it < input.end(); ++it) {
+        charIndex = tolower(*it) - 'a';
+        if ((charIndex >= 0) && (charIndex <= 25 )) {
+            counts[charIndex]++;
+            //cout << to_string(charIndex) << " " << *it << endl;
+        }
+    }
+
+    return (--counter);
 }
 
 int main(void)
@@ -85,14 +102,14 @@ int main(void)
     string prefix="Joe Boyle's challenge is to construct a sentence which contains ";
     unsigned char alphaCount[NUMCHARS] = { 0 };
     string testString;
+    counter=10;
 
     init_number_names();
 
     do {
         testString = prefix + gen_string(alphaCount);
+        cout << testString << endl;
     } while (count_chars(testString, alphaCount));
-
-    cout << testString << endl;
 
     exit(EXIT_SUCCESS);
 }
